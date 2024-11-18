5627959f94c47556484fe5e2137c8a01a642b30f
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-11-18 22:49:16 +0900

    [Feat] 라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "../src/Home";
+import Login from "../src/login/Login";
+import SignUp from "../src/sign-up/SignUp";
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signUp",
+      element: <SignUp />,
+    },
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
+);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
