2107b7802e98b3792f027cb668b4e7901c429c34
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-11-19 00:16:39 +0900

    [Feat] 문항 질문 형식 만들기

--- src/write-list/WriteList.js
+++ src/write-list/WriteList.js
@@ -1,12 +1,31 @@
 import React from "react";
 import CategoryList from "./component/CategoryList";
+import QuestionTitle from "./component/QuestionTitle";
+import styled from "styled-components";
 
 const WriteList = () => {
   return (
     <div>
+      <Group>
+        <QuestionTitle text="안녕하세요, 김사자님!" />
+        <Logo />
+      </Group>
+      <QuestionTitle text="오늘은 어떤 버킷리스트를 작성할까요?" />
       <CategoryList />
     </div>
   );
 };
 
 export default WriteList;
+
+const Group = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+const Logo = styled.img.attrs({
+  src: "irume.png",
+})`
+  width: 20px;
+  height: 22px;
+`;
---
+++ src/write-list/component/QuestionTitle.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const QuestionTitle = ({ text }) => {
+  return <Title>{text}</Title>;
+};
+
+export default QuestionTitle;
+
+const Title = styled.div`
+  color: #4a4a4a;
+  font-size: 21px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 22px;
+  letter-spacing: -0.408px;
+`;
