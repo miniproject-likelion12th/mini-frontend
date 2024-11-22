e7f0575e50c5f7c3b6f35f2e1c7dfade603f5016
Youbin Jang <120731400+youbing5921@users.noreply.github.com>
2024-11-23 00:32:46 +0900

    [Feat] 버킷리스트 작성 완료 페이지 형식 구현

--- src/write-list/write-goal/WriteGoal.js
+++ src/write-list/write-goal/WriteGoal.js
@@ -1,37 +1,54 @@
 import React, { useState } from "react";
 import { Container, Wrapper } from "../style-component/Wrapper";
-import Banner from "../style-component/Banner";
-import MenuBanner from "../style-component/MenuBanner";
 import QuestionTitle from "../style-component/QuestionTitle";
 import NoticeText from "../style-component/NoticeText";
 import styled from "styled-components";
 import WarningText from "../style-component/WarningText";
 import BottomButton from "../style-component/BottomButton";
 import GoalList from "./component/GoalList";
+import ResultModal from "./component/ResultModal";
+import Banner from "../style-component/Banner";
+import MenuBanner from "../style-component/MenuBanner";
 
-const WriteGoal = ({ period, duration_years }) => {
+const WriteGoal = ({ period = "short_term", duration_years }) => {
   const [showWaring, setShowWaring] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const CreateBucket = () => {
+    setModalOpen(true);
+  };
 
   return (
-    <Container>
-      <Ask>
-        <QuestionTitle text="버킷리스트 달성을 위한" />
-      </Ask>
-      <Ask>
-        <QuestionTitle text="구체적인 목표를 작성해볼까요?" />
-        <NoticeText text="* 1개 이상 필수 입력 항목입니다." />
-      </Ask>
-      <GoalList period={period} duration_years={duration_years} />
-      <WarningText
-        text="필수 입력 항목을 1개 이상 작성해주세요."
-        display={showWaring}
-      />
-      <BottomButton text="완료" />
-    </Container>
+    <Wrapper>
+      <Banner />
+      <ModalPosition>
+        {!modalOpen && <ResultModal />}
+        <MenuBanner />
+        <Container>
+          <Ask>
+            <QuestionTitle text="버킷리스트 달성을 위한" />
+          </Ask>
+          <Ask>
+            <QuestionTitle text="구체적인 목표를 작성해볼까요?" />
+            <NoticeText text="* 1개 이상 필수 입력 항목입니다." />
+          </Ask>
+          <GoalList period={period} duration_years={duration_years} />
+          <WarningText
+            text="필수 입력 항목을 1개 이상 작성해주세요."
+            display={showWaring}
+          />
+          <BottomButton text="완료" onClick={CreateBucket} />
+        </Container>
+      </ModalPosition>
+    </Wrapper>
   );
 };
 
 export default WriteGoal;
+
+const ModalPosition = styled.div`
+  position: relative;
+`;
 
 const Ask = styled.div`
   width: 100%;
